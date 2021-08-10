536f2f5b114a931e0e9e687029e2fe3881a352e6
Ubuntu <ubuntu@ip-172-31-34-220.us-east-2.compute.internal>
2021-08-10 05:10:03 +0000

    second

---
+++ client/build/precache-manifest.471aa429e5f08f1e3c23c011b09d4772.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "037557ce1f75e0f2f97288e2129d8780",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e06cab8130e9d362db12",
+    "url": "/static/css/main.28ec31e2.chunk.css"
+  },
+  {
+    "revision": "c04d96349dc15a967ac1",
+    "url": "/static/js/2.82715dcb.chunk.js"
+  },
+  {
+    "revision": "d4773cfc8952e56a2df4dd3d885d1f23",
+    "url": "/static/js/2.82715dcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e06cab8130e9d362db12",
+    "url": "/static/js/main.b2ff45ea.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
